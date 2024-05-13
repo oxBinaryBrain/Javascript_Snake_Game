47e08f703abbbc2df994a0c746a76de027e97772
oxBinaryBrain <udayg6269@gmail.com>
2024-05-13 10:32:48 +0530

    main file added

---
+++ snake.js
@@ -0,0 +1,32 @@
+var canvas;
+var ctx;
+
+var head;
+var apple;
+var ball;
+
+var dots;
+var apple_x;
+var apple_y;
+
+var leftDirection = false;
+var rightDirection = true;
+var upDirection = false;
+var downDirection = false;
+var inGame = true;    
+
+const DOT_SIZE = 10;
+const ALL_DOTS = 900;
+const MAX_RAND = 29;
+const DELAY = 140;
+const C_HEIGHT = 300;
+const C_WIDTH = 300;    
+
+const LEFT_KEY = 37;
+const RIGHT_KEY = 39;
+const UP_KEY = 38;
+const DOWN_KEY = 40;
+
+var x = new Array(ALL_DOTS);
+var y = new Array(ALL_DOTS);   
+
