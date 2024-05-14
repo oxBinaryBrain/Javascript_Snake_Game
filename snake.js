cabbc4eff9838e277d56054cacfbc5e3f2a87bb4
oxBinaryBrain <udayg6269@gmail.com>
2024-05-14 12:04:28 +0530

    Modified snake.js

--- snake.js
+++ snake.js
@@ -61,3 +61,156 @@ function createSnake() {
         y[z] = 50;
     }
 }
+function checkApple() {
+
+    if ((x[0] == apple_x) && (y[0] == apple_y)) {
+
+        dots++;
+        locateApple();
+    }
+}    
+
+function doDrawing() {
+    
+    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+    
+    if (inGame) {
+
+        ctx.drawImage(apple, apple_x, apple_y);
+
+        for (var z = 0; z < dots; z++) {
+            
+            if (z == 0) {
+                ctx.drawImage(head, x[z], y[z]);
+            } else {
+                ctx.drawImage(ball, x[z], y[z]);
+            }
+        }    
+    } else {
+
+        gameOver();
+    }        
+}
+
+function gameOver() {
+    
+    ctx.fillStyle = 'white';
+    ctx.textBaseline = 'middle'; 
+    ctx.textAlign = 'center'; 
+    ctx.font = 'normal bold 18px serif';
+    
+    ctx.fillText('Game over', C_WIDTH/2, C_HEIGHT/2);
+}
+
+function checkApple() {
+
+    if ((x[0] == apple_x) && (y[0] == apple_y)) {
+
+        dots++;
+        locateApple();
+    }
+}
+
+function move() {
+
+    for (var z = dots; z > 0; z--) {
+        x[z] = x[(z - 1)];
+        y[z] = y[(z - 1)];
+    }
+
+    if (leftDirection) {
+        x[0] -= DOT_SIZE;
+    }
+
+    if (rightDirection) {
+        x[0] += DOT_SIZE;
+    }
+
+    if (upDirection) {
+        y[0] -= DOT_SIZE;
+    }
+
+    if (downDirection) {
+        y[0] += DOT_SIZE;
+    }
+}    
+
+function checkCollision() {
+
+    for (var z = dots; z > 0; z--) {
+
+        if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+            inGame = false;
+        }
+    }
+
+    if (y[0] >= C_HEIGHT) {
+        inGame = false;
+    }
+
+    if (y[0] < 0) {
+       inGame = false;
+    }
+
+    if (x[0] >= C_WIDTH) {
+      inGame = false;
+    }
+
+    if (x[0] < 0) {
+      inGame = false;
+    }
+}
+
+function locateApple() {
+
+    var r = Math.floor(Math.random() * MAX_RAND);
+    apple_x = r * DOT_SIZE;
+
+    r = Math.floor(Math.random() * MAX_RAND);
+    apple_y = r * DOT_SIZE;
+}    
+
+function gameCycle() {
+    
+    if (inGame) {
+
+        checkApple();
+        checkCollision();
+        move();
+        doDrawing();
+        setTimeout("gameCycle()", DELAY);
+    }
+}
+
+onkeydown = function(e) {
+    
+    var key = e.keyCode;
+    
+    if ((key == LEFT_KEY) && (!rightDirection)) {
+        
+        leftDirection = true;
+        upDirection = false;
+        downDirection = false;
+    }
+
+    if ((key == RIGHT_KEY) && (!leftDirection)) {
+        
+        rightDirection = true;
+        upDirection = false;
+        downDirection = false;
+    }
+
+    if ((key == UP_KEY) && (!downDirection)) {
+        
+        upDirection = true;
+        rightDirection = false;
+        leftDirection = false;
+    }
+
+    if ((key == DOWN_KEY) && (!upDirection)) {
+        
+        downDirection = true;
+        rightDirection = false;
+        leftDirection = false;
+    }        
+};    
